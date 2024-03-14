39bf571f8fad2be637ee0b563292bed6652e8366
Bruno <brunogteixeira18@gmail.com>
2024-03-14 19:35:22 -0300

    configuração de pastas

---
+++ SneakerHaven/src/screens/components/navbar.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { AppBar, Icon, IconButton } from "@react-native-material/core";
+
+const NavBar = () => {
+    return(
+        <AppBar
+            title="Teste"
+            centerTitle={true}
+        ></AppBar>
+    );
+};
+
+export default NavBar;
---
+++ SneakerHaven/src/screens/home/index.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { SafeAreaView } from 'react-native';
+
+import NavBar from '../components/navbar'
+
+const Home = () => {
+    return (
+        <SafeAreaView>
+            <NavBar />
+        </SafeAreaView>
+    );
+};
+
+export default Home;
